d4a02600aabd0044b4da0f7d685dd8a409df5d05
Bhavesh Ramburn <nitrogen@gmail.com>
2022-02-06 18:10:46 +0000

    feat: migration files

---
+++ migrations/sqls/20220206172441-usertables-up.sql
@@ -0,0 +1,10 @@
+/* Replace with your SQL commands */
+create table IF NOT EXISTS users
+(
+    id           int primary key,
+    firstName    varchar(128) not null,
+    lastName     varchar(128) not null,
+    email        varchar(256) not null unique,
+    passwordHash varchar(256)
+
+);
