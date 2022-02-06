673138b4c129843c53028993babb37af422cc8da
Bhavesh Ramburn <nitrogen@gmail.com>
2022-02-06 16:43:10 +0000

    feat: setup userspec

---
+++ src/tests/userSpec.ts
@@ -0,0 +1,19 @@
+import {UserModel} from "../models/UserModel"
+
+
+describe("Testing User Spec",()=>{
+    const userID=0
+    const userModel = new UserModel()
+
+    it('should create a user', function () {
+        userModel.create(
+    })
+
+
+    afterAll(()=>{
+
+
+    })
+})
+
+
