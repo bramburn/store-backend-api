adef0267d6dd65b20f63520b1b84cb35427fa0f4
Bhavesh Ramburn <nitrogen@gmail.com>
2022-02-08 21:20:01 +0000

    fix: removed init.sql but will add some checks at the end to see if we need the init.sql

--- init.sql
+++ init.sql
@@ -1,5 +1,5 @@
-CREATE USER udacity WITH PASSWORD 'udacitypassword123$';
-CREATE DATABASE udacity_store;
+CREATE USER udacity WITH PASSWORD 'Sandal5-Yesterday-Dawdler';
+CREATE DATABASE IF NOT EXISTS udacity_store;
 GRANT ALL PRIVILEGES ON DATABASE udacity_store TO udacity;
 CREATE DATABASE udacity_store_test;
 GRANT ALL PRIVILEGES ON DATABASE udacity_store_test TO udacity;
