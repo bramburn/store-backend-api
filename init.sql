fb3511432e32256200ac40df2209c71e4548e3b3
Bhavesh Ramburn <nitrogen@gmail.com>
2022-02-14 08:01:55 +0000

    feat:added sql

--- init.sql
+++ init.sql
@@ -1,5 +1,4 @@
-CREATE USER udacity WITH PASSWORD 'Sandal5-Yesterday-Dawdler';
-CREATE DATABASE IF NOT EXISTS udacity_store;
-GRANT ALL PRIVILEGES ON DATABASE udacity_store TO udacity;
-CREATE DATABASE udacity_store_test;
+CREATE DATABASE IF NOT EXISTS udacity_store_test;
 GRANT ALL PRIVILEGES ON DATABASE udacity_store_test TO udacity;
+
+-- Start seeding the test --
